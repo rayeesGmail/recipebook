c0dbbe219c7d74890cfcf59c59b91691c4f3b013
rayees <shaikhrayeesahmed@gmail.com>
2018-11-09 12:02:44 +0530

    initial commit.

---
+++ src/main/java/me/shaikhrayeesahmed/recipebook/RecipebookApplication.java
@@ -0,0 +1,12 @@
+package me.shaikhrayeesahmed.recipebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipebookApplication.class, args);
+    }
+}
